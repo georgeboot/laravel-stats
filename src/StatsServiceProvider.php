730ab317f1484f70fa03ad2d44d799a6b55ddb2a
George Boot <george@famboot.nl>
2017-10-21 14:09:31 +0200

    Support for Lumen

--- src/StatsServiceProvider.php
+++ src/StatsServiceProvider.php
@@ -2,24 +2,28 @@
 
 namespace Wnx\LaravelStats;
 
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 use Wnx\LaravelStats\Commands\StatsListCommand;
 
 class StatsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/stats.php' => config_path('stats.php'),
-            ], 'config');
+        $source = realpath($raw = __DIR__.'/../config/stats.php') ?: $raw;
+
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('stats.php')], 'config');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('stats');
         }
+
+        $this->mergeConfigFrom($source, 'stats');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/stats.php', 'stats');
-
         $this->commands([
             StatsListCommand::class,
         ]);
